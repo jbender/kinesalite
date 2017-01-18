23f937856a6f25d5fcd3a16fcf2675850a2e4c33
Jonathan Bender <jlbender@gmail.com>
2017-01-18 17:53:56 -0500

    add FirehoseDeleteDeliveryStream

--- firehose/actions/deleteDeliveryStream.js
+++ firehose/actions/deleteDeliveryStream.js
@@ -1,3 +1,29 @@
 module.exports = function deleteDeliveryStream(store, data, cb) {
-  cb({statusCode: 501, body: 'Not Yet Implemented'})
+
+  var key = data.DeliveryStreamName,
+      metaDb = store.metaDb
+
+  store.getStream(key, function(err, stream) {
+    if (err) return cb(err)
+
+    stream.DeliveryStreamStatus = 'DELETING'
+
+    metaDb.put(key, stream, function(err) {
+      if (err) return cb(err)
+
+      store.deleteStreamDb(key, function(err) {
+        if (err) return cb(err)
+
+        setTimeout(function() {
+          metaDb.del(key, function(err) {
+            if (err && !/Database is not open/.test(err))
+              console.error(err.stack || err)
+          })
+        }, store.deleteStreamMs)
+
+        cb()
+      })
+    })
+  })
+
 }
