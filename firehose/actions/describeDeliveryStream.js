0d718c3725db75fded4cd2b8671a6dead458e26b
Jonathan Bender <jlbender@gmail.com>
2017-01-20 12:34:32 -0500

    add firehose.describeDeliveryStream

--- firehose/actions/describeDeliveryStream.js
+++ firehose/actions/describeDeliveryStream.js
@@ -1,3 +1,22 @@
 module.exports = function describeDeliveryStream(requestMeta, logger, store, data, cb) {
-  cb({statusCode: 501, body: 'Not Yet Implemented'})
+
+  var actionName = 'firehose.describeDeliveryStream',
+      actionMeta = Object.assign({}, requestMeta),
+      streamName = data.DeliveryStreamName,
+      metaDb = store.metaDb
+
+  actionMeta.action = actionName
+
+  logger.verbose('action.start', actionMeta)
+
+  store.getStream(streamName, function(err, stream) {
+    if (err) {
+      logger.verbose('action.error', actionMeta)
+      return cb(err)
+    }
+
+    logger.verbose('action.complete', actionMeta)
+    cb(null, {DeliveryStreamDescription: stream})
+  })
+
 }
